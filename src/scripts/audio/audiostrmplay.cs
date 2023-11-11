6ebc2f72172aee47ad6cef130e5e4390c1d83bb7
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 20:01:05 +0000

    Create audiostrmplay.cs

---
+++ src/scripts/audio/audiostrmplay.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+
+public partial class audiostrmplay : Node
+{
+    private AudioStreamPlayer audioplayer;
+    public void PlayAudio(Node input_parent_node, AudioStreamSample input_sample, String input_bus)
+    {
+        this.audioplayer = new AudioStreamPlayer();
+        this.audioplayer.Bus = input_bus;
+        this.audioplayer.Stream = input_sample;
+        this.audioplayer.Connect("finished", this.audioplayer, "queue_free");
+        input_parent_node.AddChild(this.audioplayer);
+
+        this.audioplayer.Play();
+    }
+
+    
+}
