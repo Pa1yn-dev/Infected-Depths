9c14d9d4ed6cfb0e9e689b438a0a2ae8e7430c43
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 20:01:05 +0000

    Update

--- src/scripts/menu/misc/loading_screen.cs
+++ src/scripts/menu/misc/loading_screen.cs
@@ -9,7 +9,7 @@ public class loading_screen : CanvasLayer
     public async void SceneTransition(string input)
     {
       this.target = input;
-      this.loadingscrn_animationplyr = GetNode<AnimationPlayer>("AnimationPlayer");
+      this.loadingscrn_animationplyr = GetNode<AnimationPlayer>("Loading_Screen_AnimationPlayer");
       this.loadingscrn_animationplyr.Play("dissolve");
 
       await ToSignal(this.loadingscrn_animationplyr, "animation_finished");
