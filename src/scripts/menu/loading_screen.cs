d1721782a533a492a731eb33a62957d20cc5c31f
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 20:01:02 +0000

    Update

--- src/scripts/menu/loading_screen.cs
+++ src/scripts/menu/loading_screen.cs
@@ -1,29 +1,20 @@
 using Godot;
 using System;
 
-public class loading_scrn : CanvasLayer
+public class loading_screen : CanvasLayer
 {
     private string target;
     private AnimationPlayer loadingscrn_animationplyr;
 
-    public void SceneTransition(string input)
+    public async void SceneTransition(string input)
     {
       this.target = input;
       this.loadingscrn_animationplyr = GetNode<AnimationPlayer>("AnimationPlayer");
       this.loadingscrn_animationplyr.Play("dissolve");
-      
-    }
 
-    public void Scene_load()
-    {
-      //Called from Animation player, end of dissolve track.
+      await ToSignal(this.loadingscrn_animationplyr, "animation_finished");
       GetTree().ChangeScene(this.target);
       this.loadingscrn_animationplyr.PlayBackwards("dissolve");
-    }
       
-     
-     
-
-   
-
+    } 
 }
