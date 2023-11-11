e9021aaf4161007365dab65a20d05571ad0c4e64
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 20:00:51 +0000

    Update

--- src/scripts/player/Movement.cs
+++ src/scripts/player/Movement.cs
@@ -3,80 +3,34 @@ using System;
 
 public class Movement : KinematicBody2D
 {
-    public Vector2 player_velocity = new Vector2();
-    [Export] public float player_vhipmagnitude = 250f;
-    [Export] public float player_vadsmagnitude = 200f;
+    public Vector2 player_velocity = Vector2.Zero;
+    public Vector2 axis = Vector2.Zero;
 
-    [Export] public float player_vacceleration = 2f;
-    [Export] public float lerp_weight = 0.5f;
-
-
-    float Lerp(float player_velocity_axis, float target_value, float weight)
-    {
-        // Greg Bahm inverted lerp equation [firstfloat * weight + secondfloat * (1 - weight)]
-        // Lerp equation [firstfloat * (1 - weight) + secondfloat * weight]
-        return player_velocity_axis * (1 - weight) + target_value * weight;
-
-    }
+    [Export] public float player_vhipvelocity = 250f;
+    [Export] public float player_vadsvelocity = 200f;
+    [Export] public float lerp_weight = 0.1f;
 
     // Called when the node enters the scene tree for the first time.
     public void GetUserInput()
     {
+
         LookAt(GetGlobalMousePosition());
 
-        player_velocity = new Vector2();
+        axis.x = Input.GetActionStrength("right") - Input.GetActionStrength("left");
+        axis.y = Input.GetActionStrength("down") - Input.GetActionStrength("up");
 
-        
-        if (Input.IsActionPressed("right"))
+        if(!Input.IsActionPressed("right_click"))
         {
-            //player_velocity.x += 1;
-            player_velocity.x = Math.Min(player_velocity.x + player_vacceleration, player_vhipmagnitude);
+            axis = axis.Normalized() * player_vhipvelocity;
+
         }
-        if (Input.IsActionPressed("left"))
-        {
-            //player_velocity.x -= 1;
-            player_velocity.x = Math.Max(player_velocity.x - player_vacceleration, -player_vhipmagnitude);
-        }
-        else if (!Input.IsActionPressed("left") & !Input.IsActionPressed("right"))
-        {
-            
-            player_velocity.x = Lerp(player_velocity.x, 0, lerp_weight);
-            
-        }
-            
-        if (Input.IsActionPressed("up"))
-        {
-            //player_velocity.y -= 1;
-            player_velocity.y = Math.Max(player_velocity.y - player_vacceleration, -player_vhipmagnitude);
-            
-        }
-            
-        if (Input.IsActionPressed("down"))
-        {
-            //player_velocity.y += 1;
-            player_velocity.y = Math.Min(player_velocity.y + player_vacceleration, player_vhipmagnitude);
-            
-        }
-        else if (!Input.IsActionPressed("down") & !Input.IsActionPressed("up"))
-        {
-            player_velocity.y = Lerp(player_velocity.y, 0, lerp_weight);
-        }
-        
-        // ADS
-        if (Input.IsActionPressed("right_click"))
-        {
-            player_velocity = player_velocity * player_vadsmagnitude;
-        }
- 
         else
-        {   
-            player_velocity = player_velocity * player_vhipmagnitude;
+        {
+            axis = axis.Normalized() * player_vadsvelocity;
         }
-            
         
-        
+        player_velocity = player_velocity.LinearInterpolate(axis, lerp_weight);
 
-        
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -85,7 +39,7 @@ public override void _PhysicsProcess(float delta)
       GetUserInput();
     
       player_velocity = MoveAndSlide(player_velocity);
-      GD.Print(player_velocity);
+      //GD.Print(player_velocity);
       
   }
 }
