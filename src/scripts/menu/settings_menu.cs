6972c048054bf6df32cbd00435f63331c916459b
Peter Hamilton <peterhamilton522@Gmail.com>
2023-12-10 02:05:56 +0000

    Update

--- src/scripts/menu/settings_menu.cs
+++ src/scripts/menu/settings_menu.cs
@@ -7,15 +7,23 @@ public partial class settings_menu : Control
     private HSlider masteraudiovol_slider;
     private HSlider musicvol_slider;
     private HSlider sfxvol_slider;
+    private OptionButton windowmode_optionbutton;
     private Button save_button;
     private Button close_button;
 
     public override void _Ready()
     {
         settingsmenu_controlnode = GetNode<Control>(".");
+
         masteraudiovol_slider = GetNode<HSlider>("MarginContainer/CenterContainer/HBoxContainer/VBoxContainer/Master_Audio_Level/HSlider");
         musicvol_slider = GetNode<HSlider>("MarginContainer/CenterContainer/HBoxContainer/VBoxContainer/Music_Audio_Level/HSlider");
         sfxvol_slider = GetNode<HSlider>("MarginContainer/CenterContainer/HBoxContainer/VBoxContainer/SFX_Audio_Level/HSlider");
+
+        windowmode_optionbutton = GetNode<OptionButton>("MarginContainer/CenterContainer/HBoxContainer/VBoxContainer/Window_Mode/OptionButton");
+        windowmode_optionbutton.AddItem("Fullscreen");
+        windowmode_optionbutton.AddItem("Windowed Fullscreen");
+        windowmode_optionbutton.AddItem("Windowed");   
+        DisplayServer.WindowRequestAttention();
 
         var audiobusmanager = new audiobusmanager();
         masteraudiovol_slider.Value = audiobusmanager.GetDefaultBusVolume("Master");
@@ -45,11 +53,28 @@ public partial class settings_menu : Control
         audiobusmanager.ChangeBusVolume("Master", (float)masteraudiovol_slider.Value);
         audiobusmanager.ChangeBusVolume("Music", (float)musicvol_slider.Value);
         audiobusmanager.ChangeBusVolume("SFX", (float)sfxvol_slider.Value);
+
+        
+
+        if (windowmode_optionbutton.GetSelectedId() == 0 && !(DisplayServer.WindowGetMode() == DisplayServer.WindowMode.Fullscreen))
+        {
+            DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+        }
+
+        if (windowmode_optionbutton.GetSelectedId() == 1 && !(DisplayServer.WindowGetMode() == DisplayServer.WindowMode.Maximized))
+        {
+            DisplayServer.WindowSetMode(DisplayServer.WindowMode.Maximized);
+        }
+
+        if (windowmode_optionbutton.GetSelectedId() == 2 && !(DisplayServer.WindowGetMode() == DisplayServer.WindowMode.Windowed))
+        {
+            DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+        }
+
     }
 
     public override void _Process(double delta)
     {
-        
         GetUserInput();
         ProcessUserInteraction();
     }
